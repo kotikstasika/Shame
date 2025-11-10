b9088ec7baab7646f3250ccca9ba6e7838ba9193
Kotikstasika <chaschinkostya2@gmail.com>
2025-11-11 00:13:07 +0700

    v1.1.1

--- src/main/java/org/dimasik/shame/command/impl/Add.java
+++ src/main/java/org/dimasik/shame/command/impl/Add.java
@@ -9,6 +9,7 @@ import org.dimasik.shame.command.SubCommand;
 import org.dimasik.shame.config.Config;
 import org.dimasik.shame.modules.impl.BanModule;
 import org.dimasik.shame.utils.Pair;
+import org.dimasik.shame.utils.Parser;
 
 import java.util.List;
 
@@ -27,21 +28,25 @@ public class Add extends SubCommand {
         String playerName = args[1];
         try {
             if (Shame.getInstance().getDatabaseManager().nicknameExists(playerName).get()) {
+                if(sender instanceof Player){
+                    sender.sendMessage(Parser.color("Игрок был уже в базе и он забанен!"));
+                }
                 Shame.getInstance().getDatabaseManager().deleteNickname(playerName);
                 String reason = Config.getString("ban.reason", "30d &cОбнаружено сторонние ПО. &7&o(Ошибка? - свяжитесь с разработчиком &f&l@stickshield_ac_bot &7&o) --sender=&c&lАнтиЧит");
                 String broadcastCmd = Config.replace(Config.getString("ban.broadcast", "litebans broadcast &7[&x&F&B&0&8&0&8&lАЧ&7]&r &f%player% &fбыл наказан античитом."), new Pair<>("%player%", args[1]));
                 Player player = Bukkit.getPlayer(args[1]);
+
                 if(player == null){
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "banip " + args[1] + " " + reason);
-                    if(!broadcastCmd.isEmpty()) {
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), broadcastCmd);
-                    }
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), broadcastCmd);
                     return;
                 }
 
-                BanModule.instant.put(player, true);
                 BanModule.startBanAnimation(player, reason, true);
             } else {
+                if(sender instanceof Player){
+                    sender.sendMessage(Parser.color("Вы добавили игрока в бан, как только он ударит что либо то он будет забанен!"));
+                }
                 Shame.getInstance().getDatabaseManager().addNickname(playerName);
             }
         }
